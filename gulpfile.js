25b71806ffdc27ab1a9f379856afb20d7115aff5
Ilya Ovdin <iovdin@gmail.com>
2015-05-25 12:18:09 +0300

    automatically upload to s3

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,21 @@
 var gulp = require('gulp'),
     jisp = require('gulp-jisp'),
-    plumber = require('gulp-plumber');
+    plumber = require('gulp-plumber'),
+    fs = require('fs');
+ 
+var s3 = require('gulp-s3-upload')(JSON.parse(fs.readFileSync('./aws.json')));
+var s3options = {Bucket : "flowblock", ACL : "public-read"};
 
 gulp.task('scripts', function(){
-    gulp.src('**/*.jisp').pipe(plumber()).pipe(jisp()).pipe(gulp.dest('./build'));
+    gulp.src('*.jisp').pipe(plumber()).pipe(jisp()).pipe(gulp.dest('./build')).pipe(s3(s3options));
 });
 gulp.task('html', function(){
-    gulp.src('./index.html').pipe(gulp.dest('./build'));
+    gulp.src('./index.html').pipe(gulp.dest('./build')).pipe(s3(s3options));
 });
 
 gulp.task('watch', function() {
-  gulp.watch("**/*.jisp", ['scripts']);
+  gulp.watch("*.jisp", ['scripts']);
   gulp.watch("./index.html", ['html']);
 });
 
-gulp.task('default', ["watch", "scripts", "html"])
+gulp.task('default', ["watch", "scripts", "html"]);
